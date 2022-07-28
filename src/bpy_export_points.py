f020540126cdc9e142dd6ca3bec6263308e73d6a
Andarael <josuraad@gmail.com>
2022-07-28 16:08:15 +0200

    fixed radius from geo nodes

--- src/bpy_export_points.py
+++ src/bpy_export_points.py
@@ -5,11 +5,11 @@ import numpy as np
 output_path = "G:/Mon Drive/Scolaire/M1_Limoges/stage M1/Work/test.txt"
 
 density = 100
-radius_min = 0.01
-radius_max = 0.05
+radius_min = 0.1
+radius_max = 1.0
 
-duplicate = True # keep a copy of the mesh
-apply_all = True # apply all modifier before generating point cloud
+duplicate = True  # keep a copy of the mesh
+apply_all = True  # apply all modifier before generating point cloud
 
 # ==============================================================================
 
@@ -19,6 +19,7 @@ D = bpy.data
 # modifier & node names
 geo_node_name = 'Distribute Points'
 geo_node_mod_name = 'Distribute Points Modifier'
+scale_attribute_name = "scale"
 
 if (duplicate):
     bpy.ops.object.duplicate()
@@ -34,14 +35,18 @@ if (apply_all):
 geo_node_mod = selected_object.modifiers.new(geo_node_mod_name, 'NODES')
 geo_node_mod.node_group = bpy.data.node_groups[geo_node_name]
 geo_node_mod["Input_3"] = float(density)
+geo_node_mod["Input_4"] = float(radius_min)
+geo_node_mod["Input_5"] = float(radius_max)
+geo_node_mod["Output_6_attribute_name"] = scale_attribute_name
 bpy.ops.object.modifier_apply(modifier=geo_node_mod_name)
 
 # move the points cloud above the surface according to particle radius
 verts = selected_object.data.vertices
 output = []
-for v in verts:
+for i, v in enumerate(verts):
     radius = np.random.uniform(radius_min, radius_max)
-    v.co += v.normal*radius
+    #v.co += v.normal*radius
+    radius = selected_object.data.attributes['scale'].data[i].value
     data = (v.co[0], v.co[1], v.co[2], radius)
     output.append(data)
 
